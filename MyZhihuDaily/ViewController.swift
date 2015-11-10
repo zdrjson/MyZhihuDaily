cc054997bacc368fa690666a0ad10eb141fd02a3
张德荣 <zhangderong@zhangderongdeMacBook-Pro.local>
2015-11-10 10:32:55 +0800

    project init pod install

---
+++ MyZhihuDaily/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyZhihuDaily
+//
+//  Created by 张德荣 on 15/11/10.
+//  Copyright © 2015年 张德荣. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
